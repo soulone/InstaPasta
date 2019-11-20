cb275409e25a7eba690490893396c373bcd5606e
soulone <ascf24032@gmail.com>
2019-11-20 11:42:37 -0500

    Initial Commit

---
+++ InstaPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaPost
+//
+//  Created by Antony Sebastian Cueva Fajardo on 11/20/19.
+//  Copyright © 2019 Antony Sebastian Cueva Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
