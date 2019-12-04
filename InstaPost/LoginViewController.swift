d49a8849cb9007c1486f1a31706436cf443f4cc0
soulone <ascf24032@gmail.com>
2019-12-04 11:55:01 -0500

    Add LoginView
    Add LoginView Basic

---
+++ InstaPost/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  InstaPost
+//
+//  Created by Antony Sebastian Cueva Fajardo on 12/4/19.
+//  Copyright © 2019 Antony Sebastian Cueva Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
